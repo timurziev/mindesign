10cf3bef9eacbde95e9cfe81a3f5d68fdbd71d42
timurziev <storona77@gmail.com>
2018-09-19 14:01:53 +0300

    add categories

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($alias = null)
     {
         $url = file_get_contents('https://markethot.ru/export/bestsp');
         $data = json_decode($url, true);
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,12 @@
         @foreach($products as $product)
             <div class="mb-3 mt-5">
                 <h1><a style="color: #1b497b" href="">{{ $product['title'] }}</a></h1>
-                <img class="mr-3" style="width: 50%;" src="{{ $product['image'] }}">
+                    <ul class="list-inline">Категории:
+                        @foreach($product['categories'] as $category)
+                            <a href="{{ url("category") . '/' . $category['alias'] }}"><li class="list-inline-item pr-2">{{ $category['title'] }}</li></a>
+                        @endforeach
+                    </ul>
+                <img class="mr-3" style="width: 20%;" src="{{ $product['image'] }}">
                 <div>
                     {!! $product['description'] !!}
                 </div>
